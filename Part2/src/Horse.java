8d8822de12f32a84bb169e383ace7b01faacc242
NikTop <69432883+N1kTop@users.noreply.github.com>
2024-04-20 11:44:27 +0100

    Accessor methods update

--- Part2/src/Horse.java
+++ Part2/src/Horse.java
@@ -19,10 +19,10 @@ public class Horse
     private String coatColor;
     private String accessory;
     //Statistics:
-    private int total_wins;
-    private int total_races;
-    private int total_distance;
-    private int total_time;
+    private int total_wins = 0;
+    private int total_races = 0;
+    private int total_distance = 0;
+    private int total_time = 0;
     private ArrayList<Integer> finishing_times;
 
 
@@ -113,14 +113,14 @@ public class Horse
     public void setCoatColor(String newColor) {coatColor = newColor;}
     public void setAccessory(String newAcc) {coatColor = accessory = newAcc;}
 
+    public void addTotal_distance(int plusDistance) {total_distance += plusDistance;}
+    public void addTotal_time(int plusTime) {total_time += plusTime;}
     public int getTotal_wins() {return total_wins;}
     public int getTotal_races() {return total_races;}
     public int getTotal_distance() {return total_distance;}
     public int getTotal_time() {return total_time;}
     public double getAverage_speed() {return (double) total_distance / (double) total_time;}
 
-    public void setTotal_wins(int newWins) {total_wins = newWins;}
-    public void setTotal_races(int newRaces) {total_races = newRaces;}
     public void incTotal_wins() {total_wins++;}
     public void incTotal_races() {total_races++;}
     public double getWinRate() {return (double) total_wins / (double) total_races;}
