021328c44f7b967287a7707e1607a1f5527fd533
NikTop <69432883+N1kTop@users.noreply.github.com>
2024-04-20 09:20:13 +0100

    5 horses

--- Part2/src/Main.java
+++ Part2/src/Main.java
@@ -3,10 +3,14 @@ public class Main {
         Horse horse1 = new Horse('♘', "Anya", 0.3);
         Horse horse2 = new Horse('♞', "Oliver", 0.5);
         Horse horse3 = new Horse('♕', "#32424", 0.8);
-        Race race1 = new Race(10);
+        Horse horse4 = new Horse('X', "Xero", 0.6);
+        Horse horse5 = new Horse('Q', "Qill", 0.4);
+        Race race1 = new Race(10, 5);
         race1.addHorse(horse1, 1);
         race1.addHorse(horse2, 2);
         race1.addHorse(horse3, 3);
+        race1.addHorse(horse4, 4);
+        race1.addHorse(horse5, 5);
         race1.startRace();
 
         Race race2 = new Race(30);
