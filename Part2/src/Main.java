82d7e45f33426e82dc833b96e0d54c33d480bcf6
NikTop <69432883+N1kTop@users.noreply.github.com>
2024-04-20 09:24:16 +0100

    name change

--- Part2/src/Main.java
+++ Part2/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Horse horse1 = new Horse('♘', "Anya", 0.3);
         Horse horse2 = new Horse('♞', "Oliver", 0.5);
         Horse horse3 = new Horse('♕', "#32424", 0.8);
-        Horse horse4 = new Horse('X', "Xero", 0.6);
+        Horse horse4 = new Horse('♔', "King", 0.6);
         Horse horse5 = new Horse('Q', "Qill", 0.4);
         Race race1 = new Race(30, 5);
         race1.addHorse(horse1, 1);
@@ -13,7 +13,7 @@ public class Main {
         race1.addHorse(horse5, 5);
         race1.startRace();
 
-        Race race2 = new Race(30);
+        Race race2 = new Race(15);
         race2.addHorse(horse1, 1);
         race2.addHorse(horse2, 2);
         race2.addHorse(horse3, 3);
