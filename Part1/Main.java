d3fc2e0549b81e3bb3054a3d1ad47d459f8a8f6b
NikTop <69432883+N1kTop@users.noreply.github.com>
2024-04-26 18:28:51 +0100

    comments

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,8 +1,18 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Main class that uses Race and Horse classes to perform races
+ */
 public class Main {
 
+    /**
+     * Prints message and receives user input.
+     * Input validation is present.
+     *
+     * @param message to be printed before waiting for input
+     * @return number entered by the user
+     */
     public static int inputInt(String message) {
         System.out.print(message);
         try {
@@ -13,21 +23,35 @@ public class Main {
             return -1;
         }
     }
+
+    /**
+     * Endlessly performs races, the race length is chosen by the user.
+     * Enter 0 to exit.
+     *
+     * @param args arguments
+     */
     public static void main(String[] args) {
         int distance;
         Race race;
+
+        //create horses
         Horse horse1 = new Horse('♘', "Anya", 0.4);
         Horse horse2 = new Horse('♞', "Oliver", 0.5);
         Horse horse3 = new Horse('♔', "King", 0.6);
+
+        //input race length
         while ((distance = inputInt("Enter Race Length (0 to exit): ")) > 0) {
             if (distance > 200) {
                 System.out.println("Too large distance");
                 continue;
             }
+            //create race and add horses
             race = new Race(distance);
             race.addHorse(horse1, 1);
             race.addHorse(horse2, 2);
             race.addHorse(horse3, 3);
+
+            //run race
             race.startRace();
         }
     }
