61a375518a3c1a7b88d76ccc148bb1d5e6030a4e
NikTop <69432883+N1kTop@users.noreply.github.com>
2024-04-26 18:23:46 +0100

    comments

--- Part2/src/Main.java
+++ Part2/src/Main.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 
+/**
+ * Main class that starts the program by running Menu.main();
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         Menu.main(null);
